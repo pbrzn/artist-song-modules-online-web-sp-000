54c7a3e0d735395697f706b07dbd32eeff6caf10
jolly-technologist-1233 <pbrennanmusic@gmail.com>
2021-04-05 16:02:27 +0000

    Automatically backed up by Learn

--- lib/concerns/memorable.rb
+++ lib/concerns/memorable.rb
@@ -1,17 +1,17 @@
 module Memorable
   module ClassMethods
     def reset_all
-      all.clear
+      self.all.clear
     end
 
-    def .count
-      all.count
+    def count
+      self.all.count
     end
   end
     
   module InstanceMethods
     def initialize
-      class.all << self
+      self.class.all << self
     end
   end
 end
